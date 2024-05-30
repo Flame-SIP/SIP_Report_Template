6045620a1dacae10ac31ee675a14c0f27598afcc
Flame-SIP <flame.sip@flame.edu.in>
2024-05-30 12:54:23 +0530

    Add files via upload

---
+++ sip_main.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from docxtpl import DocxTemplate
+import io
+from sip_utils import *
+
+# function to format document
+def render_docx(data):
+    doc = DocxTemplate(report_dict['SIP Report'])
+    doc.render(data)
+    return doc
+
+# set page configuration
+st.set_page_config(
+    page_title="Sample Report Generator",
+    layout="wide")
+
+# initialize variables in session state
+if 'Context' not in st.session_state:
+	st.session_state['Context'] = {'Title_of_Report' : '[Report Title]',
+	'Faculty_Advisor': '[Faculty Advisor]',
+	'Org_Advisor' : '[Organization Advisor]',
+    'Org_Name' : '[Organization Name]',
+    'Date' : '[xx Month 20xx]',
+    'Student_Name' : "[Student Name]",
+    'Student_Number' : '[Student Number]',
+    'Year' : report_year,
+    'Program' : '[Program]'}
+
+st.markdown("Enter your information here for the SIP Report Template:")
+
+# which report
+
+st.session_state['Context']['Title_of_Report'] = st.text_input(Title_of_Report['Question'], key='report_name',placeholder=Title_of_Report['placeholder'])
+
+st.session_state['Context']['Student_Name'] = st.text_input(Student_Name['Question'], key='student_name',placeholder=Student_Name['placeholder'])
+
+st.session_state['Context']['Student_Number'] = st.text_input(Student_Number['Question'], key='student_no',placeholder=Student_Number['placeholder'])
+
+st.session_state['Context']['Program'] = st.selectbox(Program['Question'], Program['options'], index=None,placeholder=Program['placeholder'])
+
+st.session_state['Context']['Date'] = st.text_input(Date['Question'], key='date',placeholder=Date['placeholder'])
+
+st.session_state['Context']['Faculty_Advisor'] = st.text_input(Faculty_Advisor['Question'], key='faculty',placeholder=Faculty_Advisor['placeholder'])
+
+st.session_state['Context']['Org_Advisor'] = st.text_input(Org_Advisor['Question'], key='org_adv',placeholder=Org_Advisor['placeholder'])
+
+st.session_state['Context']['Org_Name'] = st.text_input(Org_Name['Question'], key='org_name',placeholder=Org_Name['placeholder'])
+
+
+if st.button("Submit Form"):
+	if st.session_state['Context']['Title_of_Report']:
+		if st.session_state['Context']['Student_Name']:
+			if st.session_state['Context']['Student_Number']:
+				if st.session_state['Context']['Program']:
+					if st.session_state['Context']['Year']:
+						if st.session_state['Context']['Date']:
+							if st.session_state['Context']['Faculty_Advisor']:
+								if st.session_state['Context']['Org_Advisor']:
+									if st.session_state['Context']['Org_Name']:
+										doc = render_docx(st.session_state['Context'])
+										bio = io.BytesIO()
+										doc.save(bio)
+
+										st.download_button(label="Download Template Report",
+											data=bio.getvalue(),
+											file_name='Student_name_SIP.docx',
+											mime='docx')
+
+st.markdown("*if you do not get a button to download the template after submitting the form, please check if you have filled information in all the above mentioned fields.")
---
+++ sip_utils.py
@@ -0,0 +1,39 @@
+
+# contains the name of the template document
+report_dict = {'SIP Report':'SIP Sample report.docx'}
+
+# year to be mentioned on report
+report_year = '2022-2025'
+
+# contains the question and placeholder text for title of the report
+Title_of_Report = {'Question': 'What is the Title of your report?',
+'placeholder': '[Report Title]'}
+
+# contains the question and placeholder text for student name
+Student_Name = {'Question': 'What is your full name?',
+'placeholder': '[Student Name]'}
+
+# contains the question and placeholder text for student number
+Student_Number = {'Question': 'What is your student id number?',
+'placeholder': '[Student Number]'}
+
+# contains the question, list of available options and placeholder text for Program of student
+Program = {'Question': 'Which program are you writing this report for?',
+'options': ["UG", "PG"],
+'placeholder': "[Program]"}
+
+# contains the question and placeholder text for the date
+Date = {'Question': 'What is the date today?',
+'placeholder': 'eg: 29 May 2024'}
+
+# contains the question and placeholder text for the name of faculty advisor
+Faculty_Advisor = {'Question': 'Full name of Faculty Advisor',
+'placeholder': 'eg: Prof. Viraj Shah'}
+
+# contains the question and placeholder text for the name of organization advisor
+Org_Advisor = {'Question': 'Full name of Organization Advisor',
+'placeholder': 'eg: Mr. XYZ'}
+
+# contains the question and placeholder text for the name of organization
+Org_Name = {'Question': 'Full name of Organization',
+'placeholder': 'eg: ABC'}
